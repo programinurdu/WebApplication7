23b086c1cc95ad94b0b6ef938221c2dd29eb33e3
Admin <javed.iqbalcs@gmail.com>
2021-12-24 07:33:41 +0000

    Add project files.

---
+++ WebApplication7/Models/EF/StudentManagerContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebApplication7.Models.EF
+{
+    public partial class StudentManagerContext : DbContext
+    {
+        public StudentManagerContext()
+        {
+        }
+
+        public StudentManagerContext(DbContextOptions<StudentManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-7RQTUPT\\SQLEXPRESS2014A;Initial Catalog=StudentManager;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.HasKey(e => e.StudentId)
+                    .HasName("PK__Students__32C52B99E79D2BE3");
+
+                entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FullName).HasMaxLength(250);
+
+                entity.Property(e => e.Notes)
+                    .HasMaxLength(10)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplication7/Models/EF/Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebApplication7.Models.EF
+{
+    public partial class Students
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int? Gender { get; set; }
+        public string Notes { get; set; }
+    }
+}
